cb24ceff5a7c2f81a47d015ac7f286ba3c38b17e
Farhan Shafiq Chowdhury <yfarhana044@gmail.com>
2024-10-24 09:13:34 +0600

    w

--- trying.py
+++ trying.py
@@ -1,36 +1,30 @@
 import pygame
 
 pygame.init()
-screen_width, screen_height = 500,500
+screen_width, screen_height = 500, 500
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('color changing sprite')
+pygame.display.set_caption('Color Changing Sprite')
 
 colors = {
-
-        'red': pygame.Color('red'),
-
-        'green': pygame.Color('green'),
-
-        'blue': pygame.Color('blue'),
-
-        'yellow': pygame.Color('yellow'),
-
-        'white': pygame.Color('white')
-
+    'red': pygame.Color('red'),
+    'green': pygame.Color('green'),
+    'blue': pygame.Color('blue'),
+    'yellow': pygame.Color('yellow'),
+    'white': pygame.Color('white')
 }
 
 current_color = colors['white']
 
-x,y = 30,30
-sprite_width, sprite_height = 60,60
+x, y = 30, 30
+sprite_width, sprite_height = 60, 60
 
 clock = pygame.time.Clock()
 
-font = pygame.font.SysFont("Times new Roman",24)
+# Reduced font size
+font = pygame.font.SysFont("Times New Roman", 24)
 
-text = font.render("My first Game screen!!! MAO!", True, (158,16,16) )
-
+text = font.render("My first Game screen!!! MAO!", True, (158, 16, 16))
 
 done = False
 while not done:
@@ -45,8 +39,8 @@ while not done:
     if pressed[pygame.K_UP]: y -= 3
     if pressed[pygame.K_DOWN]: y += 3
     
-    x = min(max(0,x), screen_width - sprite_width)
-    y = min(max(0,y), screen_height - sprite_height)
+    x = min(max(0, x), screen_width - sprite_width)
+    y = min(max(0, y), screen_height - sprite_height)
     
     if x == 0: current_color = colors['blue']
     elif x == screen_width - sprite_width: current_color = colors['yellow']
@@ -54,16 +48,14 @@ while not done:
     elif y == screen_height - sprite_height: current_color = colors['green']
     else: current_color = colors['white']
     
-    screen.fill((230,230,250))
+    screen.fill((230, 230, 250))
     
     screen.blit(text, (screen_width // 2 - text.get_width() // 2, screen_height // 2 - text.get_height() // 2))
     
-    pygame.draw.rect(screen, current_color, (x,y, sprite_width, sprite_height))
+    pygame.draw.rect(screen, current_color, (x, y, sprite_width, sprite_height))
     
     pygame.display.flip()
     
     clock.tick(90)
-    
+
 pygame.quit()
-        
-    
